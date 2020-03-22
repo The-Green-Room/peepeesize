3355821b971d2292fe5613b21a68650e65ecffbb
LeadPoisoning <gschwartzenager@gmail.com>
2020-03-22 17:34:50 -0500

    made input screen

---
+++ activities/InputScreen.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class InputScreen extends Component {
+    render () {
+      return (
+        <Text>PeePee Size</Text>
+      )
+    }
+  }
+  
+  export default InputScreen;
