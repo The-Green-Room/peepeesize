83415c8887c4169f52b7be7b81cd75ba5b342467
LeadPoisoning <gschwartzenager@gmail.com>
2020-03-22 19:28:16 -0500

    starting on input line component

--- activities/InputScreen.js
+++ activities/InputScreen.js
@@ -1,10 +1,54 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, View, TextInput} from 'react-native'
+
+function InputLine(props) {
+  return (
+    <View style={{
+      flexDirection: 'row',
+      height: 40,
+      backgroundColor: 'steelblue',
+      justifyContent: 'flex-start'
+    }}>
+      <Text style={{
+        fontSize: 20
+      }}>
+        What is your dick size?
+      </Text>
+
+      <TextInput
+        keyboardType={'numeric'}
+        style={{
+          borderColor: 'gray',
+          borderWidth: 1,
+          fontSize: 20,
+        }}
+      />
+
+      <Text style={{
+        fontSize: 20
+      }}>
+        inches
+      </Text>
+    </View>
+  )
+}
 
 class InputScreen extends Component {
     render () {
+        const viewStyle = {
+          alignItems: 'center',
+          justifyContent: 'space-around'
+        }
+
+        const headerStyle = {
+          fontSize: 32,
+          fontWeight: 'bold',
+        }
       return (
-        <Text>PeePee Size</Text>
+        <View style={viewStyle}>
+          <Text style={headerStyle}>How do I compare?</Text>
+          <InputLine />
+        </View>
       )
     }
   }
